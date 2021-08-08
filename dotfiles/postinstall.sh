fd37625bf8a66c9068507dd9eb03c96d7068fac2
STRANGE <38800238+rivasjoaquin02@users.noreply.github.com>
2021-08-08 16:31:04 -0400

    Added postinstall.sh

---
+++ dotfiles/postinstall.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*- 
+
+sudo pacman -Syu
+yay -Syua
+
+
+#  Desarrollo
+sudo pacman -S base-devel git vim github-desktop gitkraken
+yay -S visual-studio-code
+
+#  Internet
+yay -S firefox microsoft-edge-beta telegram-desktop
+
+#  Utilidades 
+sudo pacman -S virtual-box veracrypt timeshift
+sudo pacman -S handbrake latte-dock    
+yay -S soundkonverter freedownloadmanager mediaelch fsearch
+
+#  Windows y Juegos
+sudo pacman -S wine wine-gecko wine-mono dxvk
+
+#  Multimedia 
+sudo pacman -S vlc mpv 
+
+#  Productividad
+sudo pacman -S korganizer calibre
+yay -S notion-app-enhanced crow-translate 
+
+
+## .dotfiles
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
